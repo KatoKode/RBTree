3018b88b0178d056522457633bac842b56cb30b5
Data Structures and Algorithms in C and Assembly Language <mcintosh1798@gmail.com>
2025-08-22 04:32:39 -0400

    Add files via upload

--- util/util.c
+++ util/util.c
@@ -1,19 +1,21 @@
 /*------------------------------------------------------------------------------
-    Red-Black-Tree Implementation in x86_64 Assembly Language with C interface
+    Red-Black-Tree (RBTREE) Implementation in x86_64 Assembly Language with
+    C interface
+
     Copyright (C) 2025  J. McIntosh
 
-    This program is free software; you can redistribute it and/or modify
+    RBTREE is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
+    RBTREE is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
+    with RBTREE; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 ------------------------------------------------------------------------------*/
 #include "util.h"
